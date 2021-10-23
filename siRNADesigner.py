3feb2eb6615313fdc3108d01a7adcfbbb616b35f
anazhmetdin <anegm98.eg@gmail.com>
2021-10-23 20:21:40 +0200

    use suitable Tm equation for short sequences

--- siRNADesigner.py
+++ siRNADesigner.py
@@ -166,120 +166,8 @@ def get_exons(Acc):
     return exons_seq
 
 
-def meltingTemp(seq, compSeq, Na=0.1, CT=1e-4, A=-10.8, R=1.987):
-    # SantaLucia & Hicks (2004), Annu. Rev. Biophys. Biomol. Struct 33: 415-440
-    nearest_neighbour = {
-        'init': (0.2, -5.7), 'init_A/T': (2.2, 6.9), 'init_G/C': (0, 0),
-        'init_oneG/C': (0, 0), 'init_allA/T': (0, 0), 'init_5T/A': (0, 0),
-        'sym': (0, -1.4),
-        'AA/TT': (-7.6, -21.3), 'AT/TA': (-7.2, -20.4), 'TA/AT': (-7.2, -20.4),
-        'CA/GT': (-8.5, -22.7), 'GT/CA': (-8.4, -22.4), 'CT/GA': (-7.8, -21.0),
-        'GA/CT': (-8.2, -22.2), 'CG/GC': (-10.6, -27.2), 'GC/CG': (-9.8, -24.4),
-        'GG/CC': (-8.0, -19.0)}
-
-    # Watkins & SantaLucia (2005), Nucl Acids Res 33: 6258-6267
-    mis_match = {
-        'AG/TT': (1.0, 0.9), 'AT/TG': (-2.5, -8.3), 'CG/GT': (-4.1, -11.7),
-        'CT/GG': (-2.8, -8.0), 'GG/CT': (3.3, 10.4), 'GG/TT': (5.8, 16.3),
-        'GT/CG': (-4.4, -12.3), 'GT/TG': (4.1, 9.5), 'TG/AT': (-0.1, -1.7),
-        'TG/GT': (-1.4, -6.2), 'TT/AG': (-1.3, -5.3), 'AA/TG': (-0.6, -2.3),
-        'AG/TA': (-0.7, -2.3), 'CA/GG': (-0.7, -2.3), 'CG/GA': (-4.0, -13.2),
-        'GA/CG': (-0.6, -1.0), 'GG/CA': (0.5, 3.2), 'TA/AG': (0.7, 0.7),
-        'TG/AA': (3.0, 7.4),
-        'AC/TT': (0.7, 0.2), 'AT/TC': (-1.2, -6.2), 'CC/GT': (-0.8, -4.5),
-        'CT/GC': (-1.5, -6.1), 'GC/CT': (2.3, 5.4), 'GT/CC': (5.2, 13.5),
-        'TC/AT': (1.2, 0.7), 'TT/AC': (1.0, 0.7),
-        'AA/TC': (2.3, 4.6), 'AC/TA': (5.3, 14.6), 'CA/GC': (1.9, 3.7),
-        'CC/GA': (0.6, -0.6), 'GA/CC': (5.2, 14.2), 'GC/CA': (-0.7, -3.8),
-        'TA/AC': (3.4, 8.0), 'TC/AA': (7.6, 20.2),
-        'AA/TA': (1.2, 1.7), 'CA/GA': (-0.9, -4.2), 'GA/CA': (-2.9, -9.8),
-        'TA/AA': (4.7, 12.9), 'AC/TC': (0.0, -4.4), 'CC/GC': (-1.5, -7.2),
-        'GC/CC': (3.6, 8.9), 'TC/AC': (6.1, 16.4), 'AG/TG': (-3.1, -9.5),
-        'CG/GG': (-4.9, -15.3), 'GG/CG': (-6.0, -15.8), 'TG/AG': (1.6, 3.6),
-        'AT/TT': (-2.7, -10.8), 'CT/GT': (-5.0, -15.8), 'GT/CT': (-2.2, -8.4),
-        'TT/AT': (0.2, -1.5),
-        'AI/TC': (-8.9, -25.5), 'TI/AC': (-5.9, -17.4), 'AC/TI': (-8.8, -25.4),
-        'TC/AI': (-4.9, -13.9), 'CI/GC': (-5.4, -13.7), 'GI/CC': (-6.8, -19.1),
-        'CC/GI': (-8.3, -23.8), 'GC/CI': (-5.0, -12.6),
-        'AI/TA': (-8.3, -25.0), 'TI/AA': (-3.4, -11.2), 'AA/TI': (-0.7, -2.6),
-        'TA/AI': (-1.3, -4.6), 'CI/GA': (2.6, 8.9), 'GI/CA': (-7.8, -21.1),
-        'CA/GI': (-7.0, -20.0), 'GA/CI': (-7.6, -20.2),
-        'AI/TT': (0.49, -0.7), 'TI/AT': (-6.5, -22.0), 'AT/TI': (-5.6, -18.7),
-        'TT/AI': (-0.8, -4.3), 'CI/GT': (-1.0, -2.4), 'GI/CT': (-3.5, -10.6),
-        'CT/GI': (0.1, -1.0), 'GT/CI': (-4.3, -12.1),
-        'AI/TG': (-4.9, -15.8), 'TI/AG': (-1.9, -8.5), 'AG/TI': (0.1, -1.8),
-        'TG/AI': (1.0, 1.0), 'CI/GG': (7.1, 21.3), 'GI/CG': (-1.1, -3.2),
-        'CG/GI': (5.8, 16.9), 'GG/CI': (-7.6, -22.0),
-        'AI/TI': (-3.3, -11.9), 'TI/AI': (0.1, -2.3), 'CI/GI': (1.3, 3.0),
-        'GI/CI': (-0.5, -1.3)}
-
-    # SantaLucia & Peyret (2001) Patent Application WO 01/94611
-    terminal_mm = {
-        'AA/TA': (-3.1, -7.8), 'TA/AA': (-2.5, -6.3), 'CA/GA': (-4.3, -10.7),
-        'GA/CA': (-8.0, -22.5),
-        'AC/TC': (-0.1, 0.5), 'TC/AC': (-0.7, -1.3), ' CC/GC': (-2.1, -5.1),
-        'GC/CC': (-3.9, -10.6),
-        'AG/TG': (-1.1, -2.1), 'TG/AG': (-1.1, -2.7), 'CG/GG': (-3.8, -9.5),
-        'GG/CG': (-0.7, -19.2),
-        'AT/TT': (-2.4, -6.5), 'TT/AT': (-3.2, -8.9), 'CT/GT': (-6.1, -16.9),
-        'GT/CT': (-7.4, -21.2),
-        'AA/TC': (-1.6, -4.0), 'AC/TA': (-1.8, -3.8), 'CA/GC': (-2.6, -5.9),
-        'CC/GA': (-2.7, -6.0), 'GA/CC': (-5.0, -13.8), 'GC/CA': (-3.2, -7.1),
-        'TA/AC': (-2.3, -5.9), 'TC/AA': (-2.7, -7.0),
-        'AC/TT': (-0.9, -1.7), 'AT/TC': (-2.3, -6.3), 'CC/GT': (-3.2, -8.0),
-        'CT/GC': (-3.9, -10.6), 'GC/CT': (-4.9, -13.5), 'GT/CC': (-3.0, -7.8),
-        'TC/AT': (-2.5, -6.3), 'TT/AC': (-0.7, -1.2),
-        'AA/TG': (-1.9, -4.4), 'AG/TA': (-2.5, -5.9), 'CA/GG': (-3.9, -9.6),
-        'CG/GA': (-6.0, -15.5), 'GA/CG': (-4.3, -11.1), ' GG/CA': (-4.6, -11.4),
-        'TA/AG': (-2.0, -4.7), 'TG/AA': (-2.4, -5.8),
-        'AG/TT': (-3.2, -8.7), 'AT/TG': (-3.5, -9.4), 'CG/GT': (-3.8, -9.0),
-        'CT/GG': (-6.6, -18.7), 'GG/CT': (-5.7, -15.9), 'GT/CG': (-5.9, -16.1),
-        'TG/AT': (-3.9, -10.5), 'TT/AG': (-3.6, -9.8)}
-
-    tmpSeq = seq
-    tmpCompSeq = compSeq
-    deltaH = 0
-    deltaS = 0
-    d_h = 0  # Names for indexes
-    d_s = 1  # 0 and 1
-
-    left_tmm = tmpCompSeq[:2][::-1] + '/' + tmpSeq[:2][::-1]
-    if left_tmm in terminal_mm:
-        deltaH += terminal_mm[left_tmm][d_h]
-        deltaS += terminal_mm[left_tmm][d_s]
-        tmpSeq = tmpSeq[1:]
-        tmpCompSeq = tmpCompSeq[1:]
-
-    right_tmm = tmpSeq[-2:] + '/' + tmpCompSeq[-2:]
-    if right_tmm in terminal_mm:
-        deltaH += terminal_mm[right_tmm][d_h]
-        deltaS += terminal_mm[right_tmm][d_s]
-        tmpSeq = tmpSeq[:-1]
-        tmpCompSeq = tmpCompSeq[:-1]
-
-    deltaH += nearest_neighbour['init_oneG/C'][d_h]
-    deltaS += nearest_neighbour['init_oneG/C'][d_s]
-
-    # Finally, the 'zipping'
-    for basenumber in range(len(tmpSeq) - 1):
-        neighbors = tmpSeq[basenumber:basenumber + 2] + \
-            '/' + tmpCompSeq[basenumber:basenumber + 2]
-        if neighbors in mis_match:
-            deltaH += mis_match[neighbors][d_h]
-            deltaS += mis_match[neighbors][d_s]
-        elif neighbors[::-1] in mis_match:
-            deltaH += mis_match[neighbors[::-1]][d_h]
-            deltaS += mis_match[neighbors[::-1]][d_s]
-        elif neighbors in nearest_neighbour:
-            deltaH += nearest_neighbour[neighbors][d_h]
-            deltaS += nearest_neighbour[neighbors][d_s]
-        elif neighbors[::-1] in nearest_neighbour:
-            deltaH += nearest_neighbour[neighbors[::-1]][d_h]
-            deltaS += nearest_neighbour[neighbors[::-1]][d_s]
-
-    Tm = ((1000 * deltaH)/(A + deltaS + R * math.log(CT / 4))) - \
-        273.15 + 16.6 * math.log10(Na)
-    return Tm
+def meltingTemp(seq):
+    return (seq.count('A')+seq.count('T')) * 2 + (seq.count('G')+seq.count('C')) * 4
 
 
 if not (accessionNumber is None):
@@ -394,8 +282,8 @@ for geneIndx in range(len(gene)):
                 seqPassenger += nucleotide
                 compSeqPassenger += compDict[nucleotide]
         if reduceOffTargets:
-            TmGuide = meltingTemp(seqGuide[1:8], compSeqGuide[1:8])
-            TmPass = meltingTemp(seqPassenger[1:8], compSeqPassenger[1:8])
+            TmGuide = meltingTemp(seqGuide[1:8])
+            TmPass = meltingTemp(seqPassenger[1:8])
             if TmPass >= 21.5 or TmGuide >= 21.5:
                 continue
             candidates[cand].append(TmGuide)
